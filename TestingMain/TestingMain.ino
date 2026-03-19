baf728738ae4ec4dd852a7c21665c6bf4f315790
aferrante <aferran5@uwo.ca>
2026-03-19 17:18:49 -0400

    Update TestingMain.ino

--- TestingMain/TestingMain.ino
+++ TestingMain/TestingMain.ino
@@ -6,9 +6,10 @@
 
 HX711 loadCell;
 
-float calibration_factor = 15000;
+float calibration_factor = 24440;
 bool running = false;  // flag for infinite testing loop
-int results[3]; // results[0], result[1], results[2]
+float results[3]; // results[0], result[1], results[2]
+char optionSelect;
 
 void setup() {
   Serial.begin(9600);
@@ -22,8 +23,6 @@ void setup() {
   Serial.println("Select user option:");
   Serial.println("1. New Patient");
   Serial.println("2. Existing Patient");
-
-  
 }
 
 void loop() {
@@ -58,44 +57,173 @@ void loop() {
     switch (testKey) {
     case '1': // pronation supination ROM
       Serial.println("Pronation / Supination Range of Motion Selected.");
-      Serial.println("Select Option");
+      Serial.println("Select Direction and Hand:");
       Serial.println("1. Pronate Left");
       Serial.println("2. Supinate Left");
       Serial.println("3. Pronate Right");
       Serial.println("4. Supinate Right");
 
-      char optionSelect = waitForKey();
+      optionSelect = waitForKey();
 
-      for (int i=0; 2; i++) {
-        Serial.println("Trial " + i + " of 3.");
+      for (int i=0; i < 3; i++) {
+        Serial.println("Trial ");
+        Serial.print(i);
+        Serial.println(" of 3");
         results[i] = proSupROM();
       }
+      if (optionSelect == 1) {
+        // save data to left pronate ROM
+      } else if (optionSelect == 2) {
+        // save data to left supinate ROM
+      } else if (optionSelect == 3) {
+        // save data to right pronate ROM
+      } else if (optionSelect == 4) {
+        // save data to right supinate ROM
+      } else {
+        Serial.println("Invalid Option");
+      }
+      break;
+
+    case '2': // pronation supination torque
+      Serial.println("Pronation / Supination Torque Selected.");
+      Serial.println("Select Direction and Hand:");
+      Serial.println("1. Pronate Left");
+      Serial.println("2. Supinate Left");
+      Serial.println("3. Pronate Right");
+      Serial.println("4. Supinate Right");
+
+      optionSelect = waitForKey();
+
+      for (int i=0; i < 3; i++) {
+        Serial.println("Trial ");
+        Serial.print(i);
+        Serial.println(" of 3");
+        results[i] = proSupF();
+      }
+      if (optionSelect == 1) {
+        // save data to left pronate torque
+      } else if (optionSelect == 2) {
+        // save data to left supinate torque
+      } else if (optionSelect == 3) {
+        // save data to right pronate torque
+      } else if (optionSelect == 4) {
+        // save data to right supinate torque
+      } else {
+        Serial.println("Invalid Option");
+      }
+      break;
+
+    case '3': // pinch strength
+      Serial.println("Pinch Grip Strength Selected.");
+      Serial.println("Select Pinch Grip Orientation and Hand");
+      Serial.println("1. Tip-to-Tip, Left");
+      Serial.println("2. Tip-to-Tip, Right");
+      Serial.println("3. Lateral Pinch, Left");
+      Serial.println("4. Lateral Pinch, Right");
+      Serial.println("5. 3-Point Pinch, Left");
+      Serial.println("6. 3-Point Pinch, Right");
+
+      optionSelect = waitForKey();
+      
+      for (int i=0; i < 3; i++) {
+        Serial.println("Trial ");
+        Serial.print(i);
+        Serial.println(" of 3");
+        results[i] = pinchTest();
+      }
+      if (optionSelect == 1) {
+        // save data to tip-to-tip, left
+      } else if (optionSelect == 2) {
+        // save data to tip-to-tip, right
+      } else if (optionSelect == 3) {
+        // save data to lateral, left
+      } else if (optionSelect == 4) {
+        // save data to lateral, right
+      } else if (optionSelect == 5) {
+        // save data to 3-point, left
+      } else if (optionSelect == 6) {
+        // save data to 3-point, right
+      } else {
+        Serial.println("Invalid Option");
+      }
+      break;
+
+    case '4': // radial ulnar deviation
+      Serial.println("Radial / Ulnar Deviation Selected.");
+      Serial.println("Select Direction and Hand:");
+      Serial.println("1. Radial, Left");
+      Serial.println("2. Ulnar, Left");
+      Serial.println("3. Radial, Right");
+      Serial.println("4. Ulnar, Right");
+
+      optionSelect = waitForKey();
+
+      for (int i=0; i < 3; i++) {
+        Serial.println("Trial ");
+        Serial.print(i);
+        Serial.println(" of 3");
+        results[i] = radialUlnar();
+      }
+      if (optionSelect == 1) {
+        // save data to left radial
+      } else if (optionSelect == 2) {
+        // save data to left ulnar
+      } else if (optionSelect == 3) {
+        // save data to right radial
+      } else if (optionSelect == 4) {
+        // save data to right ulnar
+      } else {
+        Serial.println("Invalid Option");
+      }
+      break;
+
+    case '5': // wrist flexion and extension
+      Serial.println("Wrist Flexion / Extension Selected.");
+      Serial.println("Select Direction and Hand:");
+      Serial.println("1. Flexion, Left");
+      Serial.println("2. Extension, Left");
+      Serial.println("3. Flexion, Right");
+      Serial.println("4. Extension, Right");
+
+      optionSelect = waitForKey();
 
       if (optionSelect == 1) {
-        // save data to left pronate
+        for (int i=0; i < 3; i++) {
+          Serial.println("Trial ");
+          Serial.print(i);
+          Serial.println(" of 3");
+          results[i] = flexExtLeft();
+        }
+        // save data to left flexion
       } else if (optionSelect == 2) {
-        // save data to left supinate
-      } else if (optionSelect == 2) {
-        // save data to right pronate
-      } else if (optionSelect == 2) {
-        // save data to right supinate
+        for (int i=0; i < 3; i++) {
+          Serial.println("Trial ");
+          Serial.print(i);
+          Serial.println(" of 3");
+          results[i] = flexExtLeft();
+        }
+        // save data to left extension
+      } else if (optionSelect == 3) {
+        for (int i=0; i < 3; i++) {
+          Serial.println("Trial ");
+          Serial.print(i);
+          Serial.println(" of 3");
+          results[i] = flexExtRight();
+        }
+        // save data to right flexion
+      } else if (optionSelect == 4) {
+        for (int i=0; i < 3; i++) {
+          Serial.println("Trial ");
+          Serial.print(i);
+          Serial.println(" of 3");
+          results[i] = flexExtRight();
+        }
+        // save data to right extension
       } else {
-        "Invalid Selection"
+        Serial.println("Invalid Option");
       }
+      break;
 
-      break;
-    case '2': // pronation supination torque
-      Serial.println("Existing Patient");
-      break;
-    case '3': // pinch strength
-      Serial.println("New Patient");
-      break;
-    case '4': // radial ulnar deviation
-      Serial.println("Existing Patient");
-      break;
-    case '5': // wrist flexion and extension
-      Serial.println("New Patient");
-      break;
     case 'x': // exit testing, select new patient
       Serial.println("Exiting Test...");
       running = false;
